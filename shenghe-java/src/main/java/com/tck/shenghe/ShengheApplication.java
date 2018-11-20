b76b9ea5bd80248fba493c53ad0f18fc108db727
tck8888 <380054728@qq.com>
2018-11-20 22:02:53 +0800

    init java project

---
+++ shenghe-java/src/main/java/com/tck/shenghe/ShengheApplication.java
@@ -0,0 +1,12 @@
+package com.tck.shenghe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShengheApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShengheApplication.class, args);
+    }
+}
