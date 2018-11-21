9c5ca2a1df35602ee4f87c95a779c888155a7cc4
tck <tck@dra100.com>
2018-11-21 12:56:01 +0800

    订单的dao测试

--- shenghe-java/src/test/java/com/tck/shenghe/repository/OrderDetailRepositoryTest.java
+++ shenghe-java/src/test/java/com/tck/shenghe/repository/OrderDetailRepositoryTest.java
@@ -1,9 +1,13 @@
 package com.tck.shenghe.repository;
 
+import com.tck.shenghe.dataobject.OrderDetail;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.math.BigDecimal;
 
 import static org.junit.Assert.*;
 
@@ -15,6 +19,22 @@ import static org.junit.Assert.*;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class OrderDetailRepositoryTest {
+
+    @Autowired
+    private OrderDetailRepository repository;
+
+    @Test
+    public void saveTest() {
+        OrderDetail orderDetail = new OrderDetail();
+        orderDetail.setDetailId("123456789");
+        orderDetail.setOrderId("111111111");
+        orderDetail.setProductId("1111111112");
+        orderDetail.setProductIcon("http://xxx.png");
+        orderDetail.setProductName("皮蛋瘦肉粥");
+        orderDetail.setProductPrice(new BigDecimal(1.2));
+        orderDetail.setProductQuantity(2);
+        repository.save(orderDetail);
+    }
 
     @Test
     public void findByOrderId() {
--- shenghe-java/src/test/java/com/tck/shenghe/repository/OrderMasterRepositoryTest.java
+++ shenghe-java/src/test/java/com/tck/shenghe/repository/OrderMasterRepositoryTest.java
@@ -1,9 +1,13 @@
 package com.tck.shenghe.repository;
 
+import com.tck.shenghe.dataobject.OrderMaster;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.math.BigDecimal;
 
 import static org.junit.Assert.*;
 
@@ -16,6 +20,20 @@ import static org.junit.Assert.*;
 @SpringBootTest
 public class OrderMasterRepositoryTest {
 
+    @Autowired
+    private OrderMasterRepository repository;
+
+    @Test
+    public void saveTest(){
+        OrderMaster orderMaster = new OrderMaster();
+        orderMaster.setOrderId("1234567");
+        orderMaster.setBuyerName("tck");
+        orderMaster.setBuyerPhone("10086");
+        orderMaster.setBuyerAddress("中国");
+        orderMaster.setBuyerOpenid("110110");
+        orderMaster.setOrderAmount(new BigDecimal(2.3));
+        repository.save(orderMaster);
+    }
     @Test
     public void findByBuyerOpenid() {
     }
